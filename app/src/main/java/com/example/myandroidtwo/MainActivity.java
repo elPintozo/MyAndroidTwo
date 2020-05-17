61a0d995b7a9ab7edcd3275c90dabdf791e907c9
elpintozo <ricardo.sr.lepe@gmail.com>
2020-05-17 12:53:54 -0400

    [Layout/ConstrainLayout] ejemplo de uso de constrainlayout, es sumamente personalizable, pero debido al alto producto de líneas que genera podría generar ralentizaciones en la aplicación

--- app/src/main/java/com/example/myandroidtwo/MainActivity.java
+++ app/src/main/java/com/example/myandroidtwo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.example_gridlayout);
+        setContentView(R.layout.example_contrainlayout);
 
         //AdapterItem adapter = new AdapterItem(this, R.layout.item_adapter, Item.getItems());
         //ListView listView = (ListView) findViewById(R.id.list_items);
